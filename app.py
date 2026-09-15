4bd33a95e0a23b3c3795bb3335faba3a511f02d0
Roberto <Roberto@ROBBER>
2026-09-15 13:32:00 -0600

    Re-estructuracion de proyecto

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from conexion import ConexionDB
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Ejemplo de endpoint para productos
+@app.route("/productos")
+def productos():
+    db = ConexionDB("host_azure", "usuario", "password", "nombre_bd")
+    cursor = db.obtener_cursor()
+    cursor.execute("SELECT nombre, precio FROM productos")
+    data = cursor.fetchall()
+    return {"productos": data}
+
+if __name__ == "__main__":
+    app.run()
